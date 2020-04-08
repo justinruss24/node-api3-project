05215c54d3d748ed6d551457c9536932d47f895e
Justin Russell <Justin@Justins-MacBook.local>
2020-04-08 15:30:45 -0700

    mvp done

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-
+const helmet = require('helmet');
 const server = express();
 
-// MW
-server.use(logger);
+const userRouter = require("./users/userRouter");
 
-server.get('/', (req, res) => {
+// MW
+server.use(helmet());
+server.use(express.json());
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+server.use("/api/users", logger, userRouter);
 
 //custom middleware
 
